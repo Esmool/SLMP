af5a0f1236633051a69787dc84430cfd31e7c9e2
brkp <brkp@tutanota.com>
2022-08-01 23:26:25 +0300

    unnecessary using directives

--- SLMP/Config.cs
+++ SLMP/Config.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SLMP
+﻿namespace SLMP
 {
     /// <summary>
     /// This class is intended to be passed to the `Client` class.
