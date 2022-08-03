3d652b82875ec975f8d3a53077f8e79bd9c08057
brkp <brkp@tutanota.com>
2022-08-03 10:29:21 +0300

    add the readbitdevice test

--- SLMP.Test/Program.cs
+++ SLMP.Test/Program.cs
@@ -7,6 +7,8 @@ namespace SLMP.Test {
         private Client client;
         private string ADDRESS = "192.168.3.201";
 
+        Device[] BitDevices  = { Device.X, Device.Y, Device.B, Device.M, Device.SM };
+        Device[] WordDevices = { Device.D, Device.W, Device.Z, Device.R, Device.SD, Device.ZR };
         Program()
         {
             config = new Config()
@@ -20,18 +22,22 @@ namespace SLMP.Test {
         public static void Main(string[] args)
         {
             Log(LogType.INFO, "Test programi baslatiliyor");
-            var program = new Program();
-            program.Connect();
+            Program program = new Program();
+            program.Connect(); Console.WriteLine("-----");
+            program.ReadBitDevices(); Console.WriteLine("-----");
         }
 
         private void ExitTest()
         {
             Log(LogType.INFO, "Test programi bitiriliyor");
+            System.Environment.Exit(0);
         }
 
+        /// baglanti testi
         private void Connect()
         {
             Log(LogType.INFO, "PLC'e baglanti testi baslatiliyor");
+            Log(LogType.DEBUG, $"Baglanilan adres: {ADDRESS}");
             try
             {
                 client.Connect(ADDRESS);
@@ -40,6 +46,24 @@ namespace SLMP.Test {
             {
                 Log(LogType.ERROR, $"Baglanti saglanamadi: {ex.Message}");
                 ExitTest();
+            }
+        }
+
+        private void ReadBitDevices()
+        {
+            Log(LogType.INFO, "Bit cihazlarindan okuma testi baslatiliyor");
+            foreach (Device device in BitDevices)
+            {
+                try
+                {
+                    Log(LogType.DEBUG, $"Read({device.ToString()}, addr=0, count=32)");
+                    client.ReadBitDevice(device, 0, 32);
+                }
+                catch (Exception ex)
+                {
+                    Log(LogType.ERROR, $"{device.ToString()} cihazindan okuma yapilamadi: ${ex.Message}");
+                    continue;
+                }
             }
         }
 
